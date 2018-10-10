387a2b41c16073d66a493b465a1f5784d4f0406c
Hongjun He <anthonyho02015@gmail.com>
2018-10-10 01:10:32 +0000

    input works, working on the computer

--- js/script.js
+++ js/script.js
@@ -2,10 +2,49 @@
 //******************TEST EARLY AND OFTEN USING console.log() ******************
 //****************** SERIOUSLY TEST USING console.log()!!! ******************
 
+/*global $*/
+
+$(document).ready(function(){
 //GLOBAL VARIABLES
 
 
 
 
 // DOCUMENT READY FUNCTION BELOW
+$("#shoot").click(function(){
+    $("#userChoice").text($("#dropdown").val());
+    //$("#computerChoice").text($("#input").val());
+    
 
+});
+
+//generate computer out put
+
+function COutPut(x,y){
+    return Math.floor(Math.random()*(x - y) + y);
+}
+
+function CoutPut2(){
+    var number = COutPut(1,3);
+    
+    switch (number){
+        case 1:
+            return "scissors";
+            break;
+            
+        case 2:
+            return "paper";
+            break;
+            
+        case 3:
+            return "rock";
+            break;
+        default:
+        return ";-;";
+    }
+    
+}
+
+
+
+});
